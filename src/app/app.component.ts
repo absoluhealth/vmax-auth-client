5448e6ce51bbdae61c9abf4abcb16b6f32d45bf8
Parthiban R <consultant3@vmaxhealthtech.com>
2024-12-05 20:17:18 +0530

    Fixed issue in ui application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,53 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'vmax-auth-client';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      const uuid = params['uuid'];
+      if (uuid) {
+        this.router.navigate(['/home'], { queryParams: { uuid: uuid } });
+      }
+    });
+  }
 
   login() {
-
-
     const headers = new HttpHeaders({
-      'content-type':'application/json',
-      'tenant-id': 'vmax'
+      'content-type': 'application/json',
+      'tenant-id': 'vmax',
     });
-    
-    this.httpClient.get('http://localhost:3000/api/auth/login?redirectUrl=http://localhost:4200', {headers}).
-    
-    
-    subscribe((res) => {
-      console.log(res);
-    },
-    (error)=>{
-      console.error(error)
-      if(error.url != null) {
-        window.location.href = error.url;
-    }
-    }
-  );
+
+    this.httpClient
+      .get(
+        'http://localhost:3000/api/auth/login?redirectUrl=http://localhost:4200&appId=1',
+        { headers }
+      )
+      .subscribe(
+        (res) => {
+          console.log(res);
+        },
+        (error) => {
+          console.error(error);
+          if (error.url != null) {
+            window.location.href = error.url;
+          }
+        }
+      );
   }
 
-  logout() {
-
-  }
+  logout() {}
 }
-
